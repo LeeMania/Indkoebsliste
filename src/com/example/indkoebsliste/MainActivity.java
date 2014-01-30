4d53d6cc47c52eb5fa7e4a3170aa87953bc5e911
Lasse Mejsner <lmejsner@hotmail.com>
2014-01-30 13:01:41 +0100

    Knap virker

--- src/com/example/indkoebsliste/MainActivity.java
+++ src/com/example/indkoebsliste/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.indkoebsliste;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	private Button btnOpret;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btnOpret = (Button) findViewById(R.id.btnOpret);
+		
+		btnOpret.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Intent i = new Intent(getApplicationContext(), OpretListe.class);
+				startActivity(i);
+			}
+		});
 	}
 //Test
 	@Override
