e57fd84379590e5fc18e08ac740f86fccb7a540d
Lasse Mejsner <lmejsner@hotmail.com>
2014-01-30 16:09:07 +0100

    Med prompt

--- gen/com/example/indkoebsliste/R.java
+++ gen/com/example/indkoebsliste/R.java
@@ -24,17 +24,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080008;
         public static final int btnOpret=0x7f080000;
         public static final int btnTilf=0x7f080004;
         public static final int eTNavn=0x7f080003;
         public static final int lVTlft=0x7f080005;
+        public static final int promptIn=0x7f080007;
+        public static final int promptTV=0x7f080006;
         public static final int relativeLayout1=0x7f080001;
         public static final int tVNavn=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_opret_liste=0x7f030001;
+        public static final int tilfoj_prompt=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/indkoebsliste/OpretListe.java
+++ src/com/example/indkoebsliste/OpretListe.java
@@ -2,7 +2,11 @@ package com.example.indkoebsliste;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,6 +19,7 @@ public class OpretListe extends Activity {
 	private Button btnTilf;
 	private EditText eTNavn;
 	private ListView lVTlft;
+	private Context context = this;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +29,42 @@ public class OpretListe extends Activity {
 		eTNavn = (EditText) findViewById(R.id.eTNavn);
 		lVTlft = (ListView) findViewById(R.id.lVTlft);
 		
-		
 		btnTilf.setOnClickListener(new OnClickListener() {
 			
 			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(getApplicationContext(), MainActivity.class);
-				parseTilDb();
-				
-				startActivity(i);
+			public void onClick(View arg0) {				
+				LayoutInflater layIn = LayoutInflater.from(context);
+				View prompt = layIn.inflate(R.layout.tilfoj_prompt,  null);
+				AlertDialog.Builder alertDiaBuild = new AlertDialog.Builder(context);
+				alertDiaBuild.setView(prompt);
+				EditText in = (EditText) prompt.findViewById(R.id.promptIn);
+				alertDiaBuild
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+//                                    	input.getText();
+                                    }
+                                })
+                        .setNegativeButton("Annuller",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+					
 			}
 		});
+//		btnOpret.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				Intent i = new Intent(getApplicationContext(), MainActivity.class);
+//				parseTilDb();
+//				
+//				startActivity(i);
+//			}
+//		});
 	}
 
 	private void parseTilDb() {
