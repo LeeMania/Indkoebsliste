3531fc360237a6b0acfcb52c912f1d25cd35c5e5
Lasse Mejsner <lmejsner@hotmail.com>
2014-01-30 13:14:09 +0100

    Test

--- src/com/example/indkoebsliste/MainActivity.java
+++ src/com/example/indkoebsliste/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		btnOpret = (Button) findViewById(R.id.btnOpret);
+		//Test
 		
 		btnOpret.setOnClickListener(new OnClickListener() {
 			
