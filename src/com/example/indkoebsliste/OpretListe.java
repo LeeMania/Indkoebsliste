2f5a34c70100f443cd1d73719d1ca8f7907a7d41
Lasse Mejsner <lmejsner@hotmail.com>
2014-01-31 10:16:43 +0100

    Update

--- src/com/example/indkoebsliste/OpretListe.java
+++ src/com/example/indkoebsliste/OpretListe.java
@@ -1,5 +1,8 @@
 package com.example.indkoebsliste;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -10,16 +13,17 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.ScrollView;
 
 public class OpretListe extends Activity {
-	private Button btnTilf;
+	private Button btnTilf, btnOpret, btnSort;
 	private EditText eTNavn;
 	private ListView lVTlft;
 	private Context context = this;
+	private ArrayAdapter<String> arrAdap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,53 +32,97 @@ public class OpretListe extends Activity {
 		btnTilf = (Button) findViewById(R.id.btnTilf);
 		eTNavn = (EditText) findViewById(R.id.eTNavn);
 		lVTlft = (ListView) findViewById(R.id.lVTlft);
-		
-		btnTilf.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {				
-				LayoutInflater layIn = LayoutInflater.from(context);
-				View prompt = layIn.inflate(R.layout.tilfoj_prompt,  null);
-				AlertDialog.Builder alertDiaBuild = new AlertDialog.Builder(context);
-				alertDiaBuild.setView(prompt);
-				EditText in = (EditText) prompt.findViewById(R.id.promptIn);
-				alertDiaBuild
-                        .setCancelable(false)
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-//                                    	input.getText();
-                                    }
-                                })
-                        .setNegativeButton("Annuller",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        dialog.cancel();
-                                    }
-                                });
+		btnOpret = (Button) findViewById(R.id.btnOpret);
+		btnSort = (Button) findViewById(R.id.btnSort);
 
-					
+		arrAdap = new ArrayAdapter<String>(this, R.layout.row);
+
+		btnTilf.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				tilfjPrompt();
 			}
 		});
-//		btnOpret.setOnClickListener(new OnClickListener() {
-//			
-//			@Override
-//			public void onClick(View v) {
-//				Intent i = new Intent(getApplicationContext(), MainActivity.class);
-//				parseTilDb();
-//				
-//				startActivity(i);
-//			}
-//		});
+
+		btnOpret.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				opret();
+			}
+		});
+
+		btnSort.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				sort();
+			}
+		});
 	}
 
-	private void parseTilDb() {
-		String navn = eTNavn.getText().toString();
-		//Hent alle textviews
-		for (int i = 0; i < lVTlft.getCount(); i++) {			
-			lVTlft.getItemAtPosition(i).toString();
+	protected void sort() {
+		ArrayList<String> listData = new ArrayList<String>();
+
+		for (int i = 0; i < lVTlft.getCount(); i++) {
+			listData.add(lVTlft.getItemAtPosition(i).toString());
 		}
 		
-		//parse data
+		Collections.sort(listData);
+		
+		arrAdap.clear();
+		
+		for (int i = 0; i < listData.size(); i++) {
+			arrAdap.add(listData.get(i));
+		}
+		
+		lVTlft.setAdapter(arrAdap);
+	}
+
+	private void tilfjPrompt() {
+		LayoutInflater layIn = LayoutInflater.from(context);
+		View prompt = layIn.inflate(R.layout.tilfoj_prompt, null);
+		AlertDialog.Builder alertDiaBuild = new AlertDialog.Builder(context);
+		alertDiaBuild.setView(prompt);
+		final EditText in = (EditText) prompt.findViewById(R.id.promptIn);
+		alertDiaBuild
+				.setCancelable(false)
+				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						tilfjTilListe(in.getText().toString());
+					}
+				})
+				.setNegativeButton("Annuller",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								dialog.cancel();
+							}
+						});
+		AlertDialog alertD = alertDiaBuild.create();
+
+		alertD.show();
+	}
+
+	protected void tilfjTilListe(String string) {
+		arrAdap.add(string);
+		lVTlft.setAdapter(arrAdap);
+
+	}
+
+	private void opret() {
+		String navn = eTNavn.getText().toString();
+		ArrayList<String> listData = new ArrayList<String>();
+
+		// Henter alle textviews
+		for (int i = 0; i < lVTlft.getCount(); i++) {
+			listData.add(lVTlft.getItemAtPosition(i).toString());
+		}
+
+		// parse navn og listData
+
+		Intent intent = new Intent(context, MainActivity.class);
+		startActivity(intent);
 	}
 
 	@Override
